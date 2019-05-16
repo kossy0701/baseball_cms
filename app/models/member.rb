6dcd817bd8195db0245fdfc33a0a4ad5c92e3575
kossy0701 <0421kossy0421@gmail.com>
2019-05-16 21:59:18 +0900

    add validation for full_name and email

--- app/models/member.rb
+++ app/models/member.rb
@@ -11,4 +11,6 @@ class Member < ApplicationRecord
     format: { with: /\A[A-Za-z][A-Za-z0-9]*\z/, allow_blank: true },
     length: { minimun: 2, maximum: 20, allow_blank: true },
     uniqueness: { case_sensitive: false } # 大文字小文字を区別しない
+  validates :full_name, presence: true, length: { maximum: 20 }
+  validates :email, email: { allow_blank: true }
 end
