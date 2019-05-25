1eda5bcef92d680a1d22237d04e638f7ca76cb83
kossy0701 <0421kossy0421@gmail.com>
2019-05-25 19:01:48 +0900

    add_and_remove_imgae_function

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,12 @@
 class ApplicationRecord < ActiveRecord::Base
+
   self.abstract_class = true
+
+  ALLOWED_CONTENT_TYPES = [
+    'image/jpeg',
+    'image/png',
+    'image/gif',
+    'image/bmp'
+  ]
+
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -7,6 +7,7 @@ class Member < ApplicationRecord
   has_many :entries, dependent: :destroy
   has_one_attached :profile_picture
   attribute :new_profile_picture
+  attribute :remove_profile_picture, :boolean
 
   has_secure_password
 
@@ -31,6 +32,18 @@ class Member < ApplicationRecord
   before_save do
     if new_profile_picture
       self.profile_picture = new_profile_picture
+    elsif remove_profile_picture
+      self.profile_picture.purge
+    end
+  end
+
+  validate if: :new_profile_picture do
+    if new_profile_picture.respond_to?(:content_type)
+      unless new_profile_picture.content_type.in?(ALLOWED_CONTENT_TYPES)
+        errors.add(:new_profile_picture, :invalid_image_type)
+      end
+    else
+      errors.add(:new_profile_picture, :invalid)
     end
   end
 
