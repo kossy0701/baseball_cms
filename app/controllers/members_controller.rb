05a1700599d0384a184ee24589a3490dce325cd8
kossy0701 <0421kossy0421@gmail.com>
2019-05-27 20:00:55 +0900

    Update members_controller.rb

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,51 +1,14 @@
 class MembersController < ApplicationController
 
-  before_action :set_member, only: [:show, :edit, :update, :destroy]
+  before_action :set_member, only: :show
 
   def index
     @member_search_form = MemberSearchForm.new member_search_params
     @members = Member.order('number').page(params[:page]).per(15)
   end
 
-  def new
-    @member = Member.new birthday: Date.new(1980, 1, 1)
-  end
-
-  def create
-    @member = Member.new member_permit_params
-    if @member.save
-      flash[:notice] = '会員を登録しました'
-      redirect_to members_path
-    else
-      flash.now[:alert] = '会員登録に失敗しました'
-      render :new
-    end
-  end
 
   def show
-  end
-
-  def edit
-  end
-
-  def update
-    if @member.update member_permit_params
-      flash[:notice] = '会員情報を更新しました'
-      redirect_to members_path
-    else
-      flash.now[:alert] = '会員情報の更新に失敗しました'
-      render :edit
-    end
-  end
-
-  def destroy
-    if @member.destroy
-      flash[:notice] = '会員を削除しました'
-      redirect_to members_path
-    else
-      flash.now[:alert] = '会員の削除に失敗しました'
-      render :new
-    end
   end
 
   def search
@@ -63,12 +26,6 @@ class MembersController < ApplicationController
 
   def member_search_params
     params.permit(:name, :full_name)
-  end
-
-  def member_permit_params
-    attrs = [:new_profile_picture, :remove_profile_picture, :number, :name, :full_name, :sex, :birthday, :prefecture_id, :email, :administrator]
-    attrs << [:password, :password_confirmation] if params[:action] == 'create'
-    params.require(:member).permit(attrs)
   end
 
 end
