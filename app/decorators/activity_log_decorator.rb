b545c88e94ad1cb96c12d0e2bef45e56b5ac3c31
kossy0701 <0421kossy0421@gmail.com>
2019-05-27 21:22:35 +0900

    add decorator method for performed_at

--- app/decorators/activity_log_decorator.rb
+++ app/decorators/activity_log_decorator.rb
@@ -1,7 +1,13 @@
 class ActivityLogDecorator < Draper::Decorator
+
   delegate_all
 
   def log_type_view
     { login: 'ログイン', logout: 'ログアウト' }[log_type.to_sym]
   end
+
+  def performed_date
+    performed_at.strftime('%Y/%m/%d %H:%M')
+  end
+
 end
