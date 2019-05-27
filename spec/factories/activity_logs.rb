c1abe8a2931f7b2a5ba12a4c948310f2f11bc797
kossy0701 <0421kossy0421@gmail.com>
2019-05-27 21:23:54 +0900

    add activity_log_spec

--- spec/factories/activity_logs.rb
+++ spec/factories/activity_logs.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :activity_log do
-    
+    log_type { [:login, :logout].sample }
+    performed_at { Date.today }
+    # performer_type member
+    # performer_id member
   end
 end
---
+++ spec/systems/activity_logs_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe 'ブログ管理機能', type: :system do
+  let(:member) { create :member, administrator: true }
+
+  before do
+    visit root_path
+    fill_in 'name', with: login_user.name
+    fill_in 'password', with: login_user.password
+    click_button 'ログイン'
+  end
+
+  describe 'アクティビティ一覧表示機能' do
+    let(:login_user) { member }
+
+    it 'アクティビティログの一覧が表示される' do
+      visit admin_activity_logs_path
+      expect(page).to have_content 'ログイン'
+    end
+  end
+end
