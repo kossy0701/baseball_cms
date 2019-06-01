b0b56a30f2cb94721005df04420625ec0952b39b
kossy0701 <0421kossy0421@gmail.com>
2019-06-01 14:00:07 +0900

    remove es6 syntax

--- app/assets/javascripts/upload_file.js
+++ app/assets/javascripts/upload_file.js
@@ -1,9 +1,9 @@
 $(document).on("change", "#member_new_profile_picture", function(e) {
-  let reader;
+  var reader;
   if (e.target.files.length) {
     reader = new FileReader;
     reader.onload = function(e) {
-      let thumbnail;
+      var thumbnail;
       thumbnail = document.getElementById('thumbnail');
       thumbnail.setAttribute('src', e.target.result);
     };
@@ -12,11 +12,11 @@ $(document).on("change", "#member_new_profile_picture", function(e) {
 });
 
 $(document).on("change", "#account_new_profile_picture", function(e) {
-  let reader;
+  var reader;
   if (e.target.files.length) {
     reader = new FileReader;
     reader.onload = function(e) {
-      let thumbnail;
+      var thumbnail;
       thumbnail = document.getElementById('thumbnail');
       thumbnail.setAttribute('src', e.target.result);
     };
@@ -25,11 +25,11 @@ $(document).on("change", "#account_new_profile_picture", function(e) {
 });
 
 $(document).on("change", "#entry_image_new_data", function(e) {
-  let reader;
+  var reader;
   if (e.target.files.length) {
     reader = new FileReader;
     reader.onload = function(e) {
-      let thumbnail;
+      var thumbnail;
       thumbnail = document.getElementById('thumbnail');
       thumbnail.setAttribute('src', e.target.result);
     };
