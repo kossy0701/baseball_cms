7e46d30ad89aacb93a988a8352470e13e8c90f2e
kossy0701 <0421kossy0421@gmail.com>
2019-06-03 20:22:42 +0900

    fix server error and add some test cases

--- spec/systems/members/activity_logs_spec.rb
+++ spec/systems/members/activity_logs_spec.rb
@@ -1,21 +1,32 @@
 require 'rails_helper'
 
-describe 'ブログ管理機能', type: :system do
-  let(:member) { create :member, administrator: true }
+describe 'アクティビティログ管理機能', type: :system do
+  describe 'ログイン済み(administrator: false)' do
+    let(:member) { create :member, administrator: false }
 
-  before do
-    visit root_path
-    fill_in 'name', with: login_user.name
-    fill_in 'password', with: login_user.password
-    click_button 'ログイン'
+    before do
+      visit root_path
+      fill_in 'name', with: login_user.name
+      fill_in 'password', with: login_user.password
+      click_button 'ログイン'
+    end
+
+    describe 'アクティビティ一覧表示機能' do
+      let(:login_user) { member }
+
+      it 'このページにはアクセスできませんと表示される' do
+        visit admin_activity_logs_path
+        expect(page).to have_content 'このページにはアクセスできません'
+      end
+    end
   end
 
-  describe 'アクティビティ一覧表示機能' do
-    let(:login_user) { member }
-
-    it 'アクティビティログの一覧が表示される' do
-      visit admin_activity_logs_path
-      expect(page).to have_content 'ログイン'
+  describe '未ログイン' do
+    describe 'アクティビティ一覧表示機能' do
+      it 'このページにはアクセスできませんと表示される' do
+        visit admin_activity_logs_path
+        expect(page).to have_content 'このページにはアクセスできません'
+      end
     end
   end
 end
