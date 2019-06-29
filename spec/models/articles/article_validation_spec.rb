780b5464a537f487874c6006fd33010437c432c6
kossy0701 <0421kossy0421@gmail.com>
2019-06-29 22:11:33 +0900

    Update article_validation_spec.rb

--- spec/models/articles/article_validation_spec.rb
+++ spec/models/articles/article_validation_spec.rb
@@ -44,4 +44,42 @@ RSpec.describe 'ArticleValidation', type: :model do
       end
     end
   end
+
+  describe 'before_validation' do
+    describe 'self.expired_at = nil if @no_expiration' do
+      it 'レコード保存時に@no_expirationがtrueならばexpired_atがnilになる' do
+        next_day = Date.today.in(1.days)
+        article = Article.new title: '練習試合の結果', body: '河川敷クリーナーズ が 6 - 4 で勝ちました。', released_at: Date.today, member_only: false, no_expiration: true, expired_at: next_day
+        expect(article.expired_at).to eq next_day
+        article.valid?
+        expect(article.expired_at).to eq nil
+      end
+
+      it 'レコード保存時に@no_expirationがfalseならばexpired_atに値が入る' do
+        next_day = Date.today.in(1.days)
+        article = Article.new title: '練習試合の結果', body: '河川敷クリーナーズ が 6 - 4 で勝ちました。', released_at: Date.today, member_only: false, no_expiration: false, expired_at: next_day
+        expect(article.expired_at).to eq next_day
+        article.valid?
+        expect(article.expired_at).to eq next_day
+      end
+    end
+  end
+
+  describe 'validate do' do
+    describe 'if expired_at && expired_at < released_at' do
+      it 'if expired_at && expired_at < released_atがtrueの場合errorを投げる' do
+        yesterday = Date.today.ago(1.days)
+        article = Article.new title: '練習試合の結果', body: '河川敷クリーナーズ が 6 - 4 で勝ちました。', released_at: Date.today, member_only: false, no_expiration: false, expired_at: yesterday
+        article.valid?
+        expect(article.errors.full_messages.join).to eq '掲載終了日時は掲載開始日より新しい日時にしてください'
+      end
+
+      it 'if expired_at && expired_at < released_atがfalseの場合エラーメッセージは空文字' do
+        yesterday = Date.today.ago(1.days)
+        article = Article.new title: '練習試合の結果', body: '河川敷クリーナーズ が 6 - 4 で勝ちました。', released_at: yesterday, member_only: false, no_expiration: false, expired_at: Date.today
+        article.valid?
+        expect(article.errors.full_messages.join).to eq ''
+      end
+    end
+  end
 end
