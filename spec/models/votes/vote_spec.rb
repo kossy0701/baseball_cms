fbe179c93a4a6f4c4c1b5b35754b5747f8a26ff0
kossy0701 <0421kossy0421@gmail.com>
2019-09-06 22:56:50 +0900

    Update vote_spec.rb

--- spec/models/votes/vote_spec.rb
+++ spec/models/votes/vote_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Vote, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:member_1) { create :member }
+  let!(:member_2) { create :member }
+  let!(:entry) { create :entry, author: member_1 }
+
+  describe 'validate' do
+    it '自分の書いた記事には投票できない' do
+      vote = Vote.new(member: member_1, entry: entry)
+      vote.valid?
+
+      expect(vote.errors[:base]).to include('は不正な値です')
+    end
+
+    it '他者の書いた記事に投票できる' do
+      vote = Vote.new(member: member_2, entry: entry)
+      vote.valid?
+
+      expect(vote.valid?).to be_truthy
+    end
+  end
 end
