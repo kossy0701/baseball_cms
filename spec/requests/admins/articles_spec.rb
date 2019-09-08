1a79a031f9ebd8161e35b3577b03d03c599ddf9e
kossy0701 <0421kossy0421@gmail.com>
2019-09-08 20:49:40 +0900

    Update articles_spec.rb

--- spec/requests/admins/articles_spec.rb
+++ spec/requests/admins/articles_spec.rb
@@ -1,79 +1,158 @@
 require 'rails_helper'
 
 describe 'Article API', type: :request do
-  let(:member) { create :member, administrator: true }
+  describe '管理者ユーザー' do
+    let(:member) { create :member, administrator: true }
 
-  before do
-    post session_url, params: { name: member.name, password: member.password }
-  end
+    before do
+      post session_url, params: { name: member.name, password: member.password }
+    end
 
-  describe 'index' do
-    it '200 OK' do
-      get admin_articles_path
+    describe 'index' do
+      it '200 OK' do
+        get admin_articles_path
 
-      expect(response.status).to eq 200
+        expect(response.status).to eq 200
+      end
+    end
+
+    describe 'new' do
+      it '200 OK' do
+        get new_admin_article_path
+
+        expect(response.status).to eq 200
+      end
+    end
+
+    describe 'create' do
+      it '302 Found' do
+        post '/admin/articles', params: { article: { title: 'test', body: 'succeess!', released_at: Date.today, no_expiration: false, member_only: false } }
+
+        expect(response.status).to eq 302
+      end
+    end
+
+    describe 'show' do
+      it '200 OK' do
+        article = create :article
+        get article_path(article)
+
+        expect(response.status).to eq 200
+      end
+    end
+
+    describe 'edit' do
+      it '200 OK' do
+        article = create :article
+        get edit_admin_article_path(article)
+
+        expect(response.status).to eq 200
+      end
+    end
+
+    describe 'update' do
+      it '302 Found' do
+        article = create :article
+        patch "/admin/articles/#{article.id}", params: { article: { title: 'test', body: 'succeess!', released_at: Date.today, no_expiration: false, member_only: false } }
+
+        expect(response.status).to eq 302
+      end
+    end
+
+    describe 'destroy' do
+      it '302 Found' do
+        article = create :article
+        delete "/admin/articles/#{article.id}"
+
+        expect(response.status).to eq 302
+      end
+    end
+
+    describe 'search' do
+      it '200 OK' do
+        article = create :article, title: 'テスト'
+        get search_admin_articles_path, params: { title: 'テスト' }
+
+        expect(response.status).to eq 200
+        expect(response.body).to include 'テスト'
+      end
     end
   end
 
-  describe 'new' do
-    it '200 OK' do
-      get new_admin_article_path
+  describe '一般ユーザー' do
+    let(:member) { create :member, administrator: false }
 
-      expect(response.status).to eq 200
+    before do
+      post session_url, params: { name: member.name, password: member.password }
     end
-  end
 
-  describe 'create' do
-    it '302 Found' do
-      post '/admin/articles', params: { article: { title: 'test', body: 'succeess!', released_at: Date.today, no_expiration: false, member_only: false } }
+    describe 'index' do
+      it '200 OK' do
+        get admin_articles_path
 
-      expect(response.status).to eq 302
+        expect(response.status).to eq 403
+      end
     end
-  end
 
-  describe 'show' do
-    it '200 OK' do
-      article = create :article
-      get article_path(article)
+    describe 'new' do
+      it '200 OK' do
+        get new_admin_article_path
 
-      expect(response.status).to eq 200
+        expect(response.status).to eq 403
+      end
     end
-  end
 
-  describe 'edit' do
-    it '200 OK' do
-      article = create :article
-      get edit_admin_article_path(article)
+    describe 'create' do
+      it '302 Found' do
+        post '/admin/articles', params: { article: { title: 'test', body: 'succeess!', released_at: Date.today, no_expiration: false, member_only: false } }
 
-      expect(response.status).to eq 200
+        expect(response.status).to eq 403
+      end
     end
-  end
 
-  describe 'update' do
-    it '302 Found' do
-      article = create :article
-      patch "/admin/articles/#{article.id}", params: { article: { title: 'test', body: 'succeess!', released_at: Date.today, no_expiration: false, member_only: false } }
+    describe 'show' do
+      it '200 OK' do
+        article = create :article
+        get admin_article_path(article)
 
-      expect(response.status).to eq 302
+        expect(response.status).to eq 403
+      end
     end
-  end
 
-  describe 'destroy' do
-    it '302 Found' do
-      article = create :article
-      delete "/admin/articles/#{article.id}"
+    describe 'edit' do
+      it '200 OK' do
+        article = create :article
+        get edit_admin_article_path(article)
 
-      expect(response.status).to eq 302
+        expect(response.status).to eq 403
+      end
     end
-  end
 
-  describe 'search' do
-    it '200 OK' do
-      article = create :article, title: 'テスト'
-      get search_admin_articles_path, params: { title: 'テスト' }
+    describe 'update' do
+      it '302 Found' do
+        article = create :article
+        patch "/admin/articles/#{article.id}", params: { article: { title: 'test', body: 'succeess!', released_at: Date.today, no_expiration: false, member_only: false } }
 
-      expect(response.status).to eq 200
-      expect(response.body).to include 'テスト'
+        expect(response.status).to eq 403
+      end
+    end
+
+    describe 'destroy' do
+      it '302 Found' do
+        article = create :article
+        delete "/admin/articles/#{article.id}"
+
+        expect(response.status).to eq 403
+      end
+    end
+
+    describe 'search' do
+      it '200 OK' do
+        article = create :article, title: 'テスト'
+        get search_admin_articles_path, params: { title: 'テスト' }
+
+        expect(response.status).to eq 403
+      end
     end
   end
 end
